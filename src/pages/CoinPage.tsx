7115a0bdf44d2b2a7ed1a28c87de8c8ae2fa336a
parthDOOM <parthivmjasoliya9@gmail.com>
2025-01-10 23:53:47 +0530

    enabled Trending Coins Section

--- src/pages/CoinPage.tsx
+++ src/pages/CoinPage.tsx
@@ -17,6 +17,7 @@ import SuggestionSection from "../components/SuggestionSection"
 function CoinPage() {
   return (
     <div className="h-full w-screen bg-slate-200/40">
+      <PageHolder/>
       <div className="w-screen lg:flex">
         <div className="lg:w-8/12 lg:ml-14 mx-4">
               <Crypto/>
@@ -29,6 +30,7 @@ function CoinPage() {
         </div>
         <div className="lg:w-4/12 lg:mr-14 mx-4">
           <CradsPromo/>
+          <TrendingCoins/>
         </div>
       </div>
       <SuggestionSection/>
