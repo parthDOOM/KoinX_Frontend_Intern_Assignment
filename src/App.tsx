8bd150b032269f3e8250975b16d9de83f7fe317c
parthDOOM <parthivmjasoliya9@gmail.com>
2025-01-10 21:03:11 +0530

    Enabled Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 //date: 10-01-2025
-// import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar"
 // import CoinPage from "./pages/CoinPage"
 
 function App() {
   return (
     <div>
-       {/* <Navbar/>
-       <CoinPage/> */}
+       <Navbar/>
+       {/* <CoinPage/> */}
     </div>
   )
 }
 
-export default App
+export default App;
