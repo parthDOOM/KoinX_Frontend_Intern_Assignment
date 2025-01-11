9c60d73cf0f39d93e99952c27beb571993ad9e42
parthDOOM <parthivmjasoliya9@gmail.com>
2025-01-11 09:13:22 +0530

    still fixing some stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/KoinX_Frontend_Intern_Assignment/',
   plugins: [react()],
 })
