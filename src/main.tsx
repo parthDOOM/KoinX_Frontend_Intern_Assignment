1e39a6811f52ae536aceeb1fb13dc0fd360bcbdd
parthDOOM <parthivmjasoliya9@gmail.com>
2025-01-11 08:53:43 +0530

    other minor fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from 'C:/Users/Admin/Desktop/Koin_X/src/App.tsx'
-import 'C:/Users/Admin/Desktop/Koin_X/src/index.css'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
